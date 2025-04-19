5537b549b0162b5f54b1eae1c1a081a126eb8dee
Nic-hub-star <nikhilchavan5566@gmail.com>
2025-04-19 00:43:39 -0700

    Add files via upload

---
+++ firebaseauth.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFDAWaD17CL-smPzuHiZUU2gDC_ZY-fRA",
+  authDomain: "login-form-31477.firebaseapp.com",
+  projectId: "login-form-31477",
+  storageBucket: "login-form-31477.firebasestorage.app",
+  messagingSenderId: "178388354466",
+  appId: "1:178388354466:web:0affc0716f170d80b15efb",
+  measurementId: "G-Q6TVKCJZE1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const email document.getElementById('email').value; 
+
+I 
+ const password document.getElementById('password').value; 
+  //submit button 
+ 
+const submit document.getElementById('submit'); 
+ submit.addEventListener("click", function (event) { 
+ event.preventDefault() 
+ alert(5) 
+
+}) 
